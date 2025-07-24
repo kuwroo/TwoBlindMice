f687ef4e6a8efada7658549e80b97b902640c55f
kuwroo <kunowro@gmail.com>
2025-07-24 23:38:12 +0800

    dialogue box logic all centralised new line use n\ new box use //

--- scenes.py
+++ scenes.py
@@ -367,7 +367,6 @@ class Entry(Scene):
                         result = npc.dialogue.handle_input(event.key)
                         if result == "CLOSE":
                             npc.showing_dialogue = False
-                            self.player.can_move = True
                         return result
 
             # Only try to interact if no dialogue is showing
